8d91aa8612afe8e0dd45d677705cb6b5abdba357
ted <tedmarov@gmail.com>
2021-03-15 13:22:33 -0500

    Made Fixtures directory

--- serverapi/models/__init__.py
+++ serverapi/models/__init__.py
@@ -1,5 +1,5 @@
 from .birdie import Birdie
 from .birdievoice import BirdieVoice
-from .category import Category
 from .comment import Comment
 from .voice import Voice
+from .voicecategory import VoiceCategory
