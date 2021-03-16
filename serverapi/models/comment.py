a5c13b02e800f184b339d30de21d6e48ac0050a1
ted <tedmarov@gmail.com>
2021-03-16 11:46:59 -0500

    seed_mockingbird works

--- serverapi/models/comment.py
+++ serverapi/models/comment.py
@@ -1,12 +1,11 @@
 """ Comment model for Mockingbird """
 
 from django.db import models
-from rest_framework.authtoken.models import Token
 
 class Comment(models.Model):
 
     comment_title = models.CharField(max_length= 50)
-    author= models.ForeignKey(Token, on_delete=models.CASCADE)
+    author= models.ForeignKey("Birdie", on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now=False, auto_now_add=False)
     voice = models.ForeignKey("Voice", on_delete=models.CASCADE)
     comment_detail = models.CharField(max_length= 250)
