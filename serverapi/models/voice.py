d8f50dca9f1f3362979ea75dd8489292f5fa6610
ted <tedmarov@gmail.com>
2021-03-25 14:23:03 -0500

    Changes

--- serverapi/models/voice.py
+++ serverapi/models/voice.py
@@ -9,6 +9,6 @@ class Voice(models.Model):
     date_created = models.DateField(auto_now_add=True, auto_now=False)
     creator = models.ForeignKey("Birdie", on_delete=models.CASCADE)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    text_id = models.CharField(max_length=3000)
+    text = models.ForeignKey("Text", on_delete=models.CASCADE)
     voice_edited = models.DateField(auto_now_add=True, auto_now=False)
     privacy = models.BooleanField(default=False)
