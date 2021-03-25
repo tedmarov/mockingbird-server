de0c0b81821febbf4f6a956487067f48824985cf
ted <tedmarov@gmail.com>
2021-03-25 11:24:06 -0500

    Loaded in ViewSets

--- serverapi/models/text.py
+++ serverapi/models/text.py
@@ -1,10 +1,11 @@
-""" Comment model for Mockingbird """
+""" Text model for Mockingbird """
 
 from django.db import models
 
-class Comment(models.Model):
+class Text(models.Model):
 
     text_title = models.CharField(max_length= 50)
+    submitter = models.ForeignKey("Birdie", on_delete=models.CASCADE)
+    edited_on = models.DateTimeField(auto_now=True, auto_now_add=False)
     text_body = models.CharField(max_length= 250)
     text_source = models.CharField(max_length= 250)
-    text_edited = models.DateTimeField(auto_now=True, auto_now_add=False)
