8bf58ac5d4af9c0817be44ee6523bfe952c68986
ted <tedmarov@gmail.com>
2021-04-01 14:06:26 -0500

    Unapplied migrations error, tried re-seeding

--- serverapi/models/text.py
+++ serverapi/models/text.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 
 class Text(models.Model):
 
-    text_title = models.CharField(max_length= 50)
+    text_title = models.CharField(max_length=50)
     submitter = models.ForeignKey(Token, on_delete=models.CASCADE)
-    text_body = models.CharField(max_length= 250)
-    text_source = models.CharField(max_length= 250)
+    text_body = models.CharField(max_length=250)
+    text_source = models.CharField(max_length=250)
--- serverapi/views/category.py
+++ serverapi/views/category.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseServerError
-from rest_framework import status, serializers
+from rest_framework import status
+from rest_framework import serializers
 from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
